9322a464a1b371d76bf5c0b63a79019b35fb3436
tatsuya.nakanishi <jwtja6wv@gmail.com>
2021-07-30 06:25:54 +0000

    API

---
+++ app/Http/Controllers/RestappController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+use App\Movie;
+
+class RestappController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(
+            [
+                'users' => $users
+            ],
+            // ステータスコードと第3引数でレスポンスヘッダーを返す
+            200,[],
+            // 日本語がそのまま表示される&一行一行分けて表示する形式にする
+            JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT
+            
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user = User::find(1);
+        $movies = $user->movies;
+        
+        $data = [
+            'movies' => $movies,
+        ];
+        
+        return view('rest.create',$data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'url' => 'required|max:11',
+            'comment' => 'max:36',
+            ]);
+        
+        User::find(1)->movies()->create([
+            'url' => $request->url,
+            'comment' => $request->comment,
+            ]);
+        
+        $movies = User::find(1)->movies;
+        
+        return response()->json(
+            [
+                'movies' => $movies
+            ],
+            // ステータスコードと第3引数でレスポンスヘッダーを返す
+            200,[],
+            // 日本語がそのまま表示される&一行一行分けて表示する形式にする
+            JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT
+            
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        $movies = $user->movies;
+        return response()->json(
+            [
+                'user' => $user
+            ],
+            // ステータスコードと第3引数でレスポンスヘッダーを返す
+            200,[],
+            // 日本語がそのまま表示される&一行一行分けて表示する形式にする
+            JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT
+            
+        );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $movie = Movie::find($id);
+        $user = $movie->user;
+        
+        if($user->id == 1){
+            $movie->delete();
+        }
+        
+        $movies = $user->movies;
+        
+        return response()->json(
+            [
+                'movies' => $movies
+            ],
+            // ステータスコードと第3引数でレスポンスヘッダーを返す
+            200,[],
+            // 日本語がそのまま表示される&一行一行分けて表示する形式にする
+            JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT
+            
+        );
+    }
+}
